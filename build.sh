823ead098e729aae647f9e97c9c4d1589965a10a
Balaji Alwar <balajialwar@berkeley.edu>
2025-06-06 14:25:20 -0700

    Removing files

--- build.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-echo "Starting webpack build..."
-cd /Users/balajialwar/Berkeley/repos/nbgitpuller-link-generator-webextension
-npx webpack --mode production --stats verbose
-echo "Build completed!"
-ls -la dist-chrome/bundle.js
-echo "Checking bundle size:"
-wc -c dist-chrome/bundle.js
